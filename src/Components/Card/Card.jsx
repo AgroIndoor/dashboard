e01e6bf64f051f2100e0bba9634edb53fa981e82
Matheus Antonelli <matheusantonelli10@gmail.com>
2023-01-25 00:58:10 -0300

    Add Components

---
+++ src/Components/Card/Card.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Card.css';
+import axios from 'axios';
+
+
+function Card({title, imageUrl, text}) {
+  return (
+    <div className='card-container'>
+        <div className="image">
+            <img src={imageUrl} alt=""/>
+        </div>
+        <div className="card-title">
+            <h3>{title}</h3>
+        </div>
+        <div className="card-text">
+           <p>{text}</p>
+        </div>
+    </div>
+  )
+}
+
+export default Card
---
+++ src/Components/Card/SensorCard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function SensorCard() {
+  return (
+    <div>SensorCard</div>
+  )
+}
+
+export default SensorCard
